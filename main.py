a35e9f7aef138a8ea58ef492ac45c85384788883
DojinP <coah2013@naver.com>
2023-03-15 13:28:51 +0900

    Master 2

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ dst1, dst2, dst3 = None, None, None # 영상처리 결과
 #메인 코드부
 src = cv2.imread('C:\GitHubLecture\images\picture11.jpg') #이미지 읽기
 
+dst1 = grayScale(src)
+dst2 = cannyEdge(src)
 
 
-cv2.imshow('source', src)
+cv2.imshow('src', src)      #화면 출력
+cv2.imshow('dst1', dst1)    #화면 출력
+cv2.imshow('dst2', dst2)    #화면 출력
+
 
 ## 마무리
 cv2.waitKey(0)
